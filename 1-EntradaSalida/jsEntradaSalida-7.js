a8766e0435cf3359e57f7a3f1feae18a4b23019a
alumno <alumno>
2019-02-08 20:33:25 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,57 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+var num1;
+var num2;
+var suma;
+
+num1=parseInt(document.getElementById("numeroUno").value);
+
+num2=parseInt(document.getElementById("numeroDos").value);
+
+suma=num1+num2;
+alert("La suma es igual a: "+suma);
 }
 
 function restar()
 {
-	
+var num1;
+var num2;
+var resta;
+
+num1=parseInt(document.getElementById("numeroUno").value);
+
+num2=parseInt(document.getElementById("numeroDos").value);
+
+resta=num1-num2;
+alert("La resta es igual a: "+resta);
 }
 
 function multiplicar()
 { 
-	
+var num1;
+var num2;
+var multiplo;
+
+num1=parseInt(document.getElementById("numeroUno").value);
+
+num2=parseInt(document.getElementById("numeroDos").value);
+
+multiplo=num1*num2;
+alert("La multiplicación es igual a: "+multiplo);
 }
 
 function dividir()
 {
-	
+var num1;
+var num2;
+var division;
+
+num1=parseInt(document.getElementById("numeroUno").value);
+
+num2=parseInt(document.getElementById("numeroDos").value);
+
+division=num1/num2;
+alert("La división es igual a: "+division);
 }
 
