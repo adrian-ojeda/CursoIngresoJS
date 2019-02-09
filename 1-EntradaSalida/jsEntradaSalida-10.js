e3cb10fb76e55413b45eebe419821bd747cbd71d
alumno <alumno>
2019-02-08 21:04:17 -0300

    Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+var num1;
+var resultado;
+
+num1=parseInt(document.getElementById("importe").value);
+
+resultado=num1-(num1*0.25);
+document.getElementById("resultado").value=resultado;	
 }
