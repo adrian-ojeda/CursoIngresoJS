c421fba5b2401833001ae14fce075020b0f34074
alumno <alumno>
2019-02-13 19:12:15 -0300

    'Update'

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad=document.getElementById("edad").value;
 
+if (edad='15'){
+    document.getElementById().value="Niña bonita"
+}else{
+    document.getElementById().value="Otra edad"
+}
 
 }//FIN DE LA FUNCIÓN
