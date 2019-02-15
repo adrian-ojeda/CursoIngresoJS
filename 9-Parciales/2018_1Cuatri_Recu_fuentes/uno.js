4a2c5042dfc809a0d141635c5912a8bd95d3a0d1
alumno <alumno>
2019-02-15 19:45:18 -0300

    Ejercicio 1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+//alert(tenes dos mascotas xxxx y xxxx , que pesa xx y xx kilos, la suma de los kilos es xxxx"")
+
+var masc1, masc2, peso1, peso2, pesototal;
+
+
+masc1=prompt("Ingresa el nombre de la primera mascota.");
+peso1=parseInt(prompt("Ingresa su peso en kilos."));
+masc2=prompt("Ingresa el nombre de la segunda mascota");
+peso2=parseInt(prompt("Ingresa su peso en kilos."));
+
+pesototal=peso1+peso2;
+
+alert("Tenes dos mascotas "+masc1+" y "+masc2+", que pesan: "+peso1+" kgs y "+peso2+" kgs, la suma de los kilos es: "+pesototal)
+
 
 }
