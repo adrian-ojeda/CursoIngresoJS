d832c56922e045d3fe68c7fbe99fff85c288ee90
alumno <alumno>
2019-02-20 19:17:13 -0300

    Update

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch(mesDelAño){
+    case "Julio":
+    case "Agosto":
+        alert("Abrigate que hace frío");
+        break;
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Julio":
+        alert("Falta para el invierno");
+        break;
+    default:
+        alert("Ya pasamos el frio, ahora calor!!!");
+    
+    
+}
 
 
 
