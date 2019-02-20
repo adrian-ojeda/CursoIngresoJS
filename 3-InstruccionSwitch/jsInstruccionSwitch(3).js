860403ca0a2002cf3d54c5039c8cdd1aee7e7e73
alumno <alumno>
2019-02-20 19:19:35 -0300

    Update

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,13 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño){
+        case "Febrero":
+            alert("Este mes tiene 29 días.");
+            break;
+        default:
+            alert("Este mes tiene 30 días o más.");
+    }
 
 
 }//FIN DE LA FUNCIÓN
