43860b8f8d5e22f5f18a672b28a9025f4778aae9
alumno <alumno>
2019-02-20 21:40:24 -0300

    Update

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var cont=0, clave = prompt("ingrese el número clave.");
+
+while(clave!="utn750"){
+    clave=prompt("Error. Ingrese el número clave nuevamente.");
+    cont++;
+        if(cont>1){
+            alert("Ud. ingresó la clave incorrecta 3 veces.")
+            break;
+        }
+            switch (clave){
+                case "utn750":
+                    alert("Bienvenido!");
+            }
+}
+
+
 
 
 }//FIN DE LA FUNCIÓN
