84dc17a3a52a42b5acd6fb3e59434f4dd640cd6a
sabrinaroldan <sabrina_roldan@outlook.com>
2019-02-22 20:14:53 -0300

    Update

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -3,9 +3,23 @@ function mostrar()
 
 	var contador=0;
 	var positivo=0;
-	var negativo=1;
-	
+  var negativo=0;
+
 	var respuesta='si';
+
+  respuesta=confirm("Quiere ingresar un número?");
+
+  //if(respuesta==true){
+  while(respuesta==true){
+    acumulador=prompt("Ingrese el número.");
+    acumulador=parseInt(acumulador);
+      if(acumulador<0){
+        negativo+=acumulador;
+      }else{
+        positivo+=acumulador;
+      }
+    respuesta=confirm("Quiere ingresar otro número?");
+}
 
 
 document.getElementById('suma').value=positivo;
