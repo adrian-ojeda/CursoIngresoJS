4f372f31b534e83ff1ec3dd7da956d93febde494
sabrinaroldan <sabrina_roldan@outlook.com>
2019-02-22 20:28:09 -0300

    Update

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,31 @@
 function mostrar()
 {
 
-	var contador=0;
+  var contador=0;
+  var minimo, maximo;
+  minimo=parseInt(minimo);
+  maximo=parseInt(maximo);
+
 	// declarar variables
-	
+
 	var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
+	respuesta=confirm("Quiere ingresar un número?");
 
+  while(respuesta==true){
+    acumulador=prompt("Ingrese el número.");
+    acumulador=parseInt(acumulador);
+      if(acumulador<acumulador){
+        minimo=acumulador;
+      }
+      if(acumulador>acumulador){
+        maximo=acumulador;
+      }
+    respuesta=confirm("Quiere ingresar otro número?");
+}
+
+document.getElementById("maximo").value=maximo;
+document.getElementById("minimo").value=minimo;
 
 
 
