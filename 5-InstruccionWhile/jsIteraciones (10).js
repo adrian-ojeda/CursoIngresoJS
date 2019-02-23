9ed4f05afdaec40eef93d5af602e9646104a6eaa
sabrinaroldan <sabrina_roldan@outlook.com>
2019-02-22 21:21:48 -0300

    Update

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,38 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
+  var contador=0,
+  var negativo, promedioneg, contneg=0, diferencia;
+  var positivo, promediopos, contpos=0;
+  var impar, par, contcero=0;
+	//declarar contadores y variables
+
 	var respuesta="si";
 
-	while(respuesta!="no")
-	{
-		
-	
-	}
+	respuesta=confirm("Quiere ingresar un número?");
+
+  do{
+    num=prompt("Ingrese el número.");
+    contador++;
+      if(num>0){
+        positivo+=num;
+        contpos++;
+        promediopos=positivo/contpos;
+          }else{
+            negativo+=num;
+            contneg++;
+            promedioneg=negativo/contneg;
+            }
+      if(num/2==0){
+        par++;
+          }else{
+            impar++;
+            }
+       if(num==0){
+         contcero++;
+       }
+      respuesta=confirm("Quiere ingresar otro número?");
+  }while(respuesta==true);
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,49 +1,31 @@
 function mostrar()
 {
-
   var contador=0;
-  //var minimo=0, maximo=0;
-
 	// declarar variables
 
 	var respuesta='si';
 
 	respuesta=confirm("Quiere ingresar un número?");
 
-/*  while(respuesta==true){
+  do{
     num=prompt("Ingrese el número.");
-    num=parseInt(num);
-      if(num<minimo){
-        minimo=parseInt(minimo);
+    contador++;
+      if(contador==1){
         minimo=num;
-      }
-      if(num>maximo){
         maximo=num;
-      }
-    respuesta=confirm("Quiere ingresar otro número?");
-}*/
+        }else{
+          if(num<minimo){
+          minimo=parseInt(minimo);
+          minimo=num;
+          }
+          if(num>maximo){
+          maximo=num;
+          }
+          }
+      respuesta=confirm("Quiere ingresar otro número?");
+  }while(respuesta==true);
 
-do{
-  num=prompt("Ingrese el número.");
-  contador++;
-    if(contador==1){
-      minimo=num;
-      maximo=num;
-      }else{
-        if(num<minimo){
-        minimo=parseInt(minimo);
-        minimo=num;
-        }
-        if(num>maximo){
-        maximo=num;
-        }
-        }
-    respuesta=confirm("Quiere ingresar otro número?");
-}while(respuesta==true);
-
-document.getElementById("maximo").value=maximo;
-document.getElementById("minimo").value=minimo;
-
-
+  document.getElementById("maximo").value=maximo;
+  document.getElementById("minimo").value=minimo;
 
 }//FIN DE LA FUNCIÓN
