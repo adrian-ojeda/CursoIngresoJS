0aa8aeb1e869681264f842fcafb61661a44875ce
alumno <alumno>
2019-02-27 19:00:44 -0300

    Update

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -22,7 +22,42 @@ function comenzar()
 
 function verificar()
 {
-	
-	
+ var num;
+  
+  num=parseInt(document.getElementById("numero").value);
+
+
+  if(numeroSecreto!=num){
+    contadorIntentos++;
+    document.getElementById("intentos").value=contadorIntentos;
+    	switch (contadorIntentos){
+			case 1:
+		  		alert("usted es un Psíquico");
+				  break;
+			case 2:
+				alert("excelente percepción");
+				break;
+			case 3:
+				alert("Esto es suerte");
+				break;
+			case 4:
+				alert("Excelente técnica");
+				break;
+			case 5:
+				alert("usted está en la media");
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				alert("falta técnica");
+				break;
+			default:
+				alert("afortunado en el amor!!");
+	  } 
+      }else{
+        alert("Usted es un ganador!!! y en solo "+contadorIntentos+" intentos.");
+  }		
 
 }
