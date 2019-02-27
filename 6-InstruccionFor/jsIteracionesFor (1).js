697bc25cd780fcbad4a88e9ab00d1c98230f42f8
alumno <alumno>
2019-02-27 19:45:44 -0300

    Update
    Signed-off-by: alumno <alumno>

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,7 +1,7 @@
 function mostrar()
 {
 
-for(var i=1; i>=0&&i<=10; i++){
+for(var i=1; i>0&&i<=10; i++){
     //alert("Los números son: "+i);
     document.write(i+", ");
 }
--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
+    var palabra;
 
+    for(var i=1;i;i++){
+        palabra=prompt("Ingrese una palabra.");
+        if(palabra=="BREAK"){
+                break;
+        }       
+}
 
 
 
