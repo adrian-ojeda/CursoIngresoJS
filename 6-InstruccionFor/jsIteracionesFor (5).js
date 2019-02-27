2f47f315c9d3f53c6d6dde1d39c060aaf8a55f47
alumno <alumno>
2019-02-27 19:48:05 -0300

    Update
    Signed-off-by: alumno <alumno>

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 
+  var num;
 
+    for(var i=1;i;i++){
+        num=prompt("Ingrese un número.");
+        num=parseInt(num);
+        if(num==9){
+                break;
+        }       
+}
 
 
 }//FIN DE LA FUNCIÓN
