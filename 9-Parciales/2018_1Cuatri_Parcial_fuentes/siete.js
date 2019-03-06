86f60e83be8028225d76bb4a1ed27a9b851b559a
sabrinaroldan <sabrina_roldan@outlook.com>
2019-03-06 01:24:38 -0300

    Update

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,39 @@
 function mostrar()
 {
+    var cont=1;
+    var notas, sexo, suma=0, promedio=0;
+    var cantvar=0;
+    var min, sexmin;
+    
 
+    do{
+        notas=prompt("Ingrese la nota del alumno: "+cont);
+        if(notas<=10&&notas>0){
+        notas=parseInt(notas);
+        }else{
+            alert("La nota debe de ser entre 0 y 10.");
+            notas=prompt("Ingrese la nota del alumno: "+cont);
+        }
+        sexo=prompt("Ingrese el sexo del alumno: "+cont);
+        if((sexo!="f")&&(sexo!="m")){
+            alert("El sexo debe ser f=femenino o m=masculino.");
+            sexo=prompt("Ingrese el sexo del alumno: "+cont);
+        }
+        if(sexo=="m"&&notas>=6){
+            cantvar++;
+        }
+        if(cont==1){
+            min=notas;
+        }
+        if(min>notas&&sexo=="m"||sexo=="f"){
+            min=notas;
+            sexmin=sexo;
+        }
+
+    suma+=notas;
+    promedio=suma/cont;    
+    cont++;
+    
+    }while (cont<=5);
+    alert("sexo: "+sexo+" notas: "+notas+" promedio: "+promedio+" minimo: "+min+" sexominimo: "+sexmin+" cantvarones: "+cantvar);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+var cont=0;
+var notas, sexo;
+
+do{
+    notas=alert("Ingrese la nota del alumno.");
+    if(notas<=10&&notas>0){
+    notas=parseInt(notas);
+    }else{
+        alert("La nota debe de ser entre 0 y 10.");
+cont++;
+}
+}while (cont<5);
 
 }
